8a4246493bd2d3b029218e975b3d7fe9c881ae5c
sumitpawar <sumitpawar@users.noreply.github.com>
2015-02-21 10:35:04 -0500

    Final IBM Model1 changes

--- IBM MODEL 1/Alignment/src/org/jhu/mt/model1/IBMModel1.java
+++ IBM MODEL 1/Alignment/src/org/jhu/mt/model1/IBMModel1.java
@@ -6,282 +6,369 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * @author sumit
+ *
+ */
 public class IBMModel1 {
+
 	// Constants for file paths 
 	// English File Path
-	private static final String enFilePath = "/home/sumit/MT/en600.468/aligner/data/hansards.e";
+	//private static final String enFilePath = "/home/sumit/MT/en600.468/aligner/data/hansards.e";
+	private static final String enFilePath = "../data/hansards.e";
 	// French File Path
-	private static final String frFilePath = "/home/sumit/MT/en600.468/aligner/data/hansards.f";
+	//private static final String frFilePath = "/home/sumit/MT/en600.468/aligner/data/hansards.f";
+	private static final String frFilePath = "../data/hansards.f";
 	// Number of lines to read
 	private static final int num_sent = 100000;
 	// (Limit) Number of Iterations
-	private static final int itr = 5;
+	private static final int itr = 2;
 	
-	// count(e|f)
-	private HashMap<String, Double>  countEF = new HashMap<String, Double>();
-	// total(f) 
-	private HashMap<String, Double>  totalF = new HashMap<String, Double>();
-	// s-total(e) 
-	private HashMap<String, Double>  stotalE = new HashMap<String, Double>();
-	// t(e|f) 
-	private HashMap<String, Double>  tEF = new HashMap<String, Double>();
+	// Map t(f|e)
+	private Map<String, Double> tFE = new HashMap<String, Double>();
+	// Map count(e,f)
+	private Map<String, Double> countEF = new HashMap<String, Double>();
+	// Map t(f) = t(f|e')
+	private Map<String, Double> tf = new HashMap<String, Double>();
+	// Map count(e) = Count(f'|e)
+	private Map<String, Double> ce = new HashMap<String, Double>();
+	// P_null (Null Probability)
+	private double p_null = 0.0;
+	
+	/**
+	 * @return the tFE
+	 */
+	public Map<String, Double> gettFE() {
+		return tFE;
+	}
+
+	/**
+	 * @param tFE the tFE to set
+	 */
+	public void settFE(Map<String, Double> tFE) {
+		this.tFE = tFE;
+	}
 
 	/**
 	 * @return the countEF
 	 */
-	public HashMap<String, Double> getCountEF() {
+	public Map<String, Double> getCountEF() {
 		return countEF;
 	}
 
 	/**
 	 * @param countEF the countEF to set
 	 */
-	public void setCountEF(HashMap<String, Double> countEF) {
+	public void setCountEF(Map<String, Double> countEF) {
 		this.countEF = countEF;
 	}
 
 	/**
-	 * @return the totalF
+	 * @return the tf
 	 */
-	public HashMap<String, Double> getTotalF() {
-		return totalF;
+	public Map<String, Double> getTf() {
+		return tf;
 	}
 
 	/**
-	 * @param totalF the totalF to set
+	 * @param tf the tf to set
 	 */
-	public void setTotalF(HashMap<String, Double> totalF) {
-		this.totalF = totalF;
+	public void setTf(Map<String, Double> tf) {
+		this.tf = tf;
 	}
 
 	/**
-	 * @return the stotalE
+	 * @return the ce
 	 */
-	public HashMap<String, Double> getStotalE() {
-		return stotalE;
+	public Map<String, Double> getCe() {
+		return ce;
 	}
 
 	/**
-	 * @param stotalE the stotalE to set
+	 * @param ce the ce to set
 	 */
-	public void setStotalE(HashMap<String, Double> stotalE) {
-		this.stotalE = stotalE;
+	public void setCe(Map<String, Double> ce) {
+		this.ce = ce;
 	}
 
 	/**
-	 * @return the tEF
+	 * @return the p_null
 	 */
-	public HashMap<String, Double> gettEF() {
-		return tEF;
+	public double getP_null() {
+		return p_null;
 	}
 
 	/**
-	 * @param tEF the tEF to set
+	 * @param p_null the p_null to set
 	 */
-	public void settEF(HashMap<String, Double> tEF) {
-		this.tEF = tEF;
+	public void setP_null(double p_null) {
+		this.p_null = p_null;
 	}
 
-	/**
-	 * Method: main()
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	// Compute Initial Counts
+	private void collectInitialCounts() {
 		File en = new File(enFilePath);
 		File fr = new File(frFilePath);
-		IBMModel1 m1 = new IBMModel1();
+		boolean print_stats = false;
 		try {
-			BufferedReader enbr = null;
-			BufferedReader frbr = null;
-			//double vocsize = 0;
+			BufferedReader enbr = new BufferedReader(new FileReader(en));
+			BufferedReader frbr = new BufferedReader(new FileReader(fr));
+			// metric --- not required though
+			//Map<String, Double> counte = new HashMap<String, Double>();
+			//Map<String, Double> countf = new HashMap<String, Double>();
+			long totale = 0, totalf = 0;
+			double null_dem = 0.0, null_num = 0.0;
 			
-			for(int i = 0; i< itr; i++) {
-				enbr = new BufferedReader(new FileReader(en));
-				frbr = new BufferedReader(new FileReader(fr));
-				String enline = null, frline = null;
-				int cnt_lines = 0;
-				m1.setStotalE(new HashMap<String, Double>());
-				while( cnt_lines <= num_sent 
-						&& (enline = enbr.readLine()) != null 
-						&& (frline = frbr.readLine()) != null ) {
-					cnt_lines ++;
-					String[] enArr = enline.trim().split(" ");
-					String[] frArr = frline.trim().split(" ");
-					
-					// Compute Normalization
-					for(String enw : enArr) {
-						for(String frw : frArr) {
-							//vocsize ++;
-							/*if((enw.equals(".") || enw.equals(",") 
-									|| enw.equals(":") || enw.equals(";") 
-									|| enw.equals("?") || enw.equals("!") 
-									|| enw.equals("(") || enw.equals(")") ) && !enw.equals(frw)) {
-								
-							}else */
-							if(null != m1.gettEF() 
-									&& !m1.gettEF().isEmpty()) {
-								String key = enw + "|" + frw;
-								if(!m1.gettEF().containsKey(key)) {
-									throw new Exception("Invalid condition :::: t(e|f) Key not found: " + key);
-								}else {
-									m1.getStotalE().put(enw, 
-										( ((m1.getStotalE().containsKey(enw))?m1.getStotalE().get(enw):0) 
-											+ m1.gettEF().get(key)));
-								}
-							}else {
-								if(null != m1.getStotalE() 
-										&& ! m1.getStotalE().isEmpty()) {
-									m1.getStotalE().put(enw, (
-										((m1.getStotalE().containsKey(enw))?m1.getStotalE().get(enw):0)  
-											//+ (1.0 / (double) frArr.length) ));
-											+ 0.01 ));
-								}else {
-									m1.getStotalE().put(enw, 0.01);//(1.0 /(double) frArr.length));
-								}
-							}
-						}
-					}
-
-				}
-					
-				// Uniformly Normalize probabilities if first iteration
-				/*if(null == m1.gettEF() 
-						|| m1.gettEF().isEmpty()) {
-					double vocabSize = m1.getStotalE().size();
-					for(Entry<String, Double> entry : m1.getStotalE().entrySet()) {
-						String key = entry.getKey();
-						//Double val = entry.getValue()/vocsize; //vocabSize;
-						Double val = entry.getValue()/vocabSize;
-						m1.getStotalE().put(key, val);
-					}
-				}*/
-				
-				// Collect Counts
-				m1.setCountEF(new HashMap<String, Double>());	// reset counts for count(e|f)
-				m1.setTotalF(new HashMap<String, Double>());	// reset counts for total(f)
-				// For the first iteration t(e|f) is not set 
-				// because we did not know the size of the vocabulary
-				if(null != m1.gettEF() && !m1.gettEF().isEmpty()) {
-					for(Entry<String, Double> entry : m1.gettEF().entrySet()) {
-						String key = entry.getKey();
-						Double val = entry.getValue();
-						String[] ef = key.split("\\|");
-						String e = ef[0], f = ef[1];
-						
-						double value = ((m1.gettEF().get(key)) / (m1.getStotalE().get(e)));
-						// count(e|f) += t(e|f) / s-total(e)
-						m1.getCountEF().put(key, 
-							((m1.getCountEF().containsKey(key))?m1.getCountEF().get(key):0)
-									+ value);
-						// total(f) += t(e|f) / s-total(e)
-						m1.getTotalF().put(f, 
-								((m1.getTotalF().containsKey(f))?m1.getTotalF().get(f):0) 
-									+ value);
-						
-					}
-				} else { // set t(e|f) = 1/|vocab|
-					//double vocabSize = m1.getStotalE().size();
-					enbr = new BufferedReader(new FileReader(en));
-					frbr = new BufferedReader(new FileReader(fr));
-					enline = null;
-					frline = null;
-					cnt_lines = 0;
-
-					while( cnt_lines <= num_sent 
-							&& (enline = enbr.readLine()) != null 
-							&& (frline = frbr.readLine()) != null ) {
-						cnt_lines ++;
-					
-						for(String enw : enline.split(" ")) {
-							for(String frw : frline.split(" ")) {
-								//double value = ( ((double) 1.0 / vocabSize) / (m1.getStotalE().get(enw)) );
-								String key = enw + "|" + frw;
-								// count(e|f) += t(e|f) / s-total(e)
-								m1.getCountEF().put(key, 
-									((m1.getCountEF().containsKey(key))?m1.getCountEF().get(key):0)
-											+ 0.01);//+ value);
-								// total(f) += t(e|f) / s-total(e)
-								m1.getTotalF().put(frw, 
-										((m1.getTotalF().containsKey(frw))?m1.getTotalF().get(frw):0) 
-											+ 0.01 ); //+ value);
-								
-							}
-						}
-					}
-				}
-					
-				
-				// Update t(e|f)
-				for(Entry<String, Double> entry : m1.getCountEF().entrySet()) {
-					String key = entry.getKey();
-					Double val = entry.getValue();
-					String f = key.split("\\|")[1];
-					m1.gettEF().put(key, (m1.getCountEF().get(key) / m1.getTotalF().get(f)) );
-				}
-				/*enbr = new BufferedReader(new FileReader(en));
-				frbr = new BufferedReader(new FileReader(fr));
-				enline = null;
-				frline = null;
-				cnt_lines = 0;
-				m1.setStotalE(new HashMap<String, Double>());
-				while( cnt_lines <= num_sent 
-						&& (enline = enbr.readLine()) != null 
-						&& (frline = frbr.readLine()) != null ) {
-					cnt_lines ++;
-
-					for(String frw : frline.trim().split(" ")) {
-						for(String enw : enline.trim().split(" ")) {
-							String key = enw + "|" + frw;
-							m1.gettEF().put(key, (m1.getCountEF().get(key) / m1.getTotalF().get(frw)) );
-						}
-					}
-					
-				}*/
-					
-			}
-			
-			// estimate probabilities
-			enbr = new BufferedReader(new FileReader(en));
-			frbr = new BufferedReader(new FileReader(fr));
 			String enline = null, frline = null;
 			int cnt_lines = 0;
-			m1.setStotalE(new HashMap<String, Double>());
-			while( cnt_lines <= 1000//num_sent 
+			while( cnt_lines <= num_sent 
 					&& (enline = enbr.readLine()) != null 
 					&& (frline = frbr.readLine()) != null ) {
 				cnt_lines ++;
-
+				
+				String[] enArr = enline.trim().toLowerCase().split("\\s+");
+				String[] frArr = frline.trim().toLowerCase().split("\\s+");
+				
+				int enlen = enArr.length;
+				int frlen = frArr.length;
+				
+				// p_null computations
+				if(enlen > frlen) {
+					null_num +=  (enlen - frlen);
+				}
+				null_dem += enlen;
+				
+				totale += enlen + 1;		// english words count & +1 for NUll word
+				totalf += frlen;			// french words count
+				// Compute Initial Counts
+				for(String frwd : frArr) {
+					//countf.put(frwd, 
+					//		((countf.containsKey(frwd))?countf.get(frwd):0.0) + 1.0);
+					tf.put(frwd, 
+							((tf.containsKey(frwd))?tf.get(frwd):0.0) + (1.0 + enlen));
+					countEF.put("null_pos=#=" + frwd, 
+							((countEF.containsKey("null_pos=#=" + frwd))?
+									countEF.get("null_pos=#=" + frwd):0.0) + 1.0);
+					ce.put("null_pos", 
+							((ce.containsKey("null_pos"))?ce.get("null_pos"):0.0) + 1.0);
+					
+					for(String enwd : enArr) {
+						ce.put(enwd, 
+								((ce.containsKey(enwd))?ce.get(enwd):0.0) + 1.0);
+						countEF.put(enwd + "=#=" + frwd, 
+								((countEF.containsKey(enwd + "=#=" + frwd))?
+										countEF.get(enwd + "=#=" + frwd):0.0) + 1.0);
+					}
+				}
+				
+			} // while loop-ends
+			p_null = null_num / null_dem;
+			
+			// print initial stats if print_stats = 1;
+			if(print_stats) {
+				System.out.println(" = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+				System.out.println("Initial stats      : ");
+				System.out.println("lines processed    : " + cnt_lines);
+				System.out.println("total french words : " + totalf);
+				System.out.println("total english words: " + totale);
+				System.out.println("total vocab count  : " + countEF.size());
+				System.out.println("t(f|e') count      : " + tf.size());
+				System.out.println("c(e|f') count      : " + ce.size());
+				System.out.println("null_num           : " + null_num);
+				System.out.println("null_dem           : " + null_dem);
+				System.out.println("p_null             : " + p_null);
+				System.out.println(" = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+			}
+			
+			// Close file read buffers
+			enbr.close();
+			frbr.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("FileNotFoundException: ");
+			System.err.println(System.getProperty("user.dir"));
+			e.printStackTrace();
+		}catch (IOException e) {
+			System.err.println("IOException: ");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Re-estimate Posterior Probability (Model);
+	// M - Step
+	private void reEstimateModel() {
+		try {
+			// re-compute model probabilities
+			if(null != countEF && !countEF.isEmpty()) {
+				tFE = new HashMap<String, Double>();
+				tf =  new HashMap<String, Double>();
+				for(Entry<String, Double> entry : countEF.entrySet()) {
+					String key = entry.getKey();
+					String[] ef = key.split("=#=");
+					String e = ef[0];
+					String f = ef[1];
+					String newKey = f + "=#=" + e;
+					double val = ( entry.getValue() / ce.get(e) );
+					tFE.put(newKey, val);
+					if(!e.equalsIgnoreCase("null_pos")) {
+						tf.put(f, 
+							( ((tf.containsKey(f))?tf.get(f):0.0) + val) );
+					}
+				}
+			}else {
+				System.err.println("Error: NULL or empty counts - countEF");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Re-estimate expectation: counts C(e,f) and C(e,f')
+	// E- Step
+	private void reEstimateCounts() {
+		try {
+			if(null != tFE && !tFE.isEmpty()) {
+				countEF = new HashMap<String, Double>();
+				ce = new HashMap<String, Double>();
+				for(Entry<String, Double> entry : tFE.entrySet()) {
+					String key = entry.getKey();
+					String[] fe = key.split("=#=");
+					String f = fe[0];
+					String e = fe[1];
+					String newKey = e + "=#=" + f;
+					double value = entry.getValue();
+					double p = 0.0;
+					
+					double den = ( (p_null * tFE.get(f + "=#=null_pos")) 
+							+ ( (1.0 - p_null) * tf.get(f)) ); 
+					if(e.equalsIgnoreCase("null_pos")) {
+						p = (p_null * value) / (den);
+					}else {
+						p = ((1.0 - p_null) * value) / (den);
+					}
+					countEF.put(newKey, 
+							( ((countEF.containsKey(newKey))?countEF.get(newKey):0.0 ) + p) );
+					ce.put(e, 
+							( ((ce.containsKey(e))?ce.get(e):0.0) + p) );
+				}
+			}else {
+				System.err.println("Error : Please set p_null and/or t(f|e) ... ");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void findOptiAlignment(Map<Integer, List<Object[]>> fval,
+			Map<Integer, Integer> femap, Map<Integer, Integer> efmap, int j) {
+		List<Object[]> jval = fval.get(j);
+		if(jval.size() > 0) {
+			Object[] val = jval.get(jval.size() - 1);
+	
+			double highest = Double.parseDouble(val[0].toString());
+			int i = Integer.parseInt(val[1].toString());
+			
+			if(efmap.containsKey(i)) { 
+				int j2 = efmap.get(i);
+				List<Object[]> jval2 = fval.get(j2);
+				if(jval2.size() > 0) {
+					Object[] val2 = jval2.get(jval2.size() - 1);
+					double highest2 = Double.parseDouble(val2[0].toString());
+					
+					if(highest >= highest2) {
+						femap.put(j, i);
+						efmap.put(i, j);
+						
+						Object[] obj = jval2.remove(jval2.size() - 1);
+						jval2.add(0, obj);
+						femap.remove(j2);
+						findOptiAlignment(fval, femap, efmap, j2);
+					}else {
+						Object[] obj = jval.remove(jval.size() - 1);
+						jval.add(0, obj);
+						findOptiAlignment(fval, femap, efmap, j);
+					}
+				}
+			}else {
+				femap.put(j, i);
+				efmap.put(i, j);
+			}
+		}else {
+			
+		}
+	}
+	
+	// Estimate word alignments
+	private void estimateAlignment() {
+		try {
+			BufferedReader enbr = new BufferedReader(
+					new FileReader(new File(enFilePath)));
+			BufferedReader frbr = new BufferedReader(
+					new FileReader(new File(frFilePath)));
+			
+			String enline = null, frline = null;
+			int cnt_lines = 0;
+			
+			while( cnt_lines <= 1000 //num_sent 
+					&& (enline = enbr.readLine()) != null 
+					&& (frline = frbr.readLine()) != null ) {
+				cnt_lines ++;
+				// if(cnt_lines ==1 ) continue; //debugging
+				
+				int frlen = frline.trim().toLowerCase().split("\\s+").length;
+				int enlen = enline.trim().toLowerCase().split("\\s+").length;
+				if(enlen < frlen) {
+					int len = frlen - enlen;
+					while (len > 0){
+						enline += " null_pos ";	
+						len --;
+					}
+				}
+				String[] frArr = frline.trim().toLowerCase().split("\\s+");
+				String[] enArr = enline.trim().toLowerCase().split("\\s+");
+				
+				Map<Integer, List<Object[]>> fval = new HashMap<Integer, List<Object[]>>();
+				Map<Integer, Integer> femap = new HashMap<Integer, Integer>();
+				Map<Integer, Integer> efmap = new HashMap<Integer, Integer>();
 				int j = 0;
-				for(String frw : frline.trim().split(" ")) {
-					int i = 0;
-					int maxi = i;
-					double max = -1.0;
-					List<Integer> maxList = new ArrayList<Integer>();
-					for(String enw : enline.trim().split(" ")) {
-						String key = enw + "|" + frw;
-						double val = m1.gettEF().get(key);
-						if(val > max) {
-							maxi = i;
-							max = val;
-							maxList.clear();
-						} else if(val == max) {
-							maxList.add(i);
-						}
+				for(String frw : frArr) {
+					int i = -1;
+					//int maxi = i;
+					//double max = -1.0;
+					List<Object[]> templist = new ArrayList<Object[]>();
+					for(String enw : enArr) {
 						i++;
+						String key = frw + "=#=" + enw;
+						double val = tFE.get(key);
+						templist.add(new Object[] {val , i});
 					}
-					System.out.print(j + "-" + maxi + " ");
-					for(Integer mi : maxList) {
-						System.out.print(j + "?" + mi + " ");
-					}
-					maxList.clear();
+					List<Object[]> d = new ArrayList<Object[]>();
+					d.addAll(templist);
+					Collections.sort(d, new Comparator<Object[]>() {
+					    public int compare(Object[] obj1, Object[] obj2) {
+					    	Double d1 = Double.parseDouble(obj1[0].toString());
+					    	Double d2 = Double.parseDouble(obj2[0].toString());
+					        return d1.compareTo(d2);
+					    }
+					});
+					fval.put(j, d);
+					findOptiAlignment(fval, femap, efmap, j);
 					j++;
 				}
-				System.out.println();
-				
+				for (int i = 0;i<femap.size();i++) {
+					if(/*null != femap.get(i) &&*/ !enArr[femap.get(i)].equalsIgnoreCase("null_pos")) {
+						System.out.print(i + "-" + femap.get(i) + " ");
+					}
+				}
+				System.out.println();	
 			}
 			
 			enbr.close();
@@ -293,10 +380,37 @@ public class IBMModel1 {
 			System.err.println("IOException: ");
 			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
-
+	
+	// Compute IBM Model1
+	private void compute_model1() {
+		try {
+			// initialize
+			collectInitialCounts(); 	// Like E - Step
+			// EM - Steps
+			for(int i = 0; i< itr; i++) {
+				reEstimateModel(); 		// M - Step 
+				reEstimateCounts(); 	// E - Step 
+			}
+			estimateAlignment();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		//IBMModel1 m1 = new IBMModel1();
+		try {
+			//m1.compute_model1();
+			long startTime = System.currentTimeMillis();
+			new IBMModel1().compute_model1();
+			long endTime = System.currentTimeMillis();
+			System.err.println("" + ((double) (endTime - startTime) / (double) (1000*60)) + " mins");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
